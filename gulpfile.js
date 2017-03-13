e73e8bbb2482dc4460d07ab80348fb48cef7fc13
Pablo-Rodriguez <pablorp1996@gmail.com>
2017-03-13 20:18:16 +0100

    initial frontend config

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+
+const gulp = require('gulp');
+const vulcanize = require('gulp-vulcanize');
+const minify = require('gulp-minify-html');
+
+gulp.task('vulcanize', () => {
+	return gulp.src('./app/frontend/components/*.html')
+      .pipe(vulcanize({
+  		inlineScripts: true,
+  		inlineCss: true
+      }))
+      .pipe(minify())
+      .pipe(gulp.dest('./app/backend/public/components/'));
+});
+
+gulp.task('html', () => {
+	return gulp.src('./app/frontend/index.html')
+		.pipe(minify())
+		.pipe(gulp.dest('./app/backend/public/'))
+});
+
+gulp.task('watch', () => {
+	gulp.watch('./app/frontend/index.html', ['html']);
+	gulp.watch('./app/frontend/components/**/*.html', ['components']);
+});
+
+gulp.task('default', ['html', 'vulcanize', 'watch']);
