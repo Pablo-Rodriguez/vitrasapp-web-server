673ed1b0efb42c4335bdb91e2359843365745614
Pablo-Rodriguez <pablorp1996@gmail.com>
2017-04-30 19:16:49 +0200

    Client-side places finished

--- app/backend/mvc/controllers/places.js
+++ app/backend/mvc/controllers/places.js
@@ -4,11 +4,38 @@ const Model = require('../business/places');
 class places {
     constructor (rex) {
         this.model = new Model(rex.getPlugin('places'));
-        rex.get('/places/by-location', this.get);
+        rex.get('/places/by-position', this.get);
     }
 
     get (req, res) {
-        res.end();
+        // TODO -> cambiar los mocks por los datos del servicio externo
+        res.json([
+            {
+                name: 'Gruas y transportes Barreiro',
+                lat: 42.2099827,
+                lng: -8.700656799999999
+            },
+            {
+                name: 'Eduardo Alonso',
+                lat: 42.20961399999999,
+                lng: -8.702802
+            },
+            {
+                name: 'A casa da Peteta',
+                lat: 42.2101944,
+                lng: -8.700333499999999
+            },
+            {
+                name: 'Carpinaval',
+                lat: 42.206927,
+                lng: -8.699814199999999
+            },
+            {
+                name: 'AUTOS ROSAS',
+                lat: 42.208653,
+                lng: -8.698027999999999
+            },
+        ]);
     }
 }
 
